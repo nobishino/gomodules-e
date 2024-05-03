10444694f97cb07c1ada3a8262611237bc71dd8a
nobishino <nobishii11@gmail.com>
2024-05-03 17:21:21 +0900

    delete dependency to f

--- e.go
+++ e.go
@@ -2,11 +2,8 @@ package e
 
 import (
 	"fmt"
-
-	f2 "github.com/nobishino/gomodules-f/f2"
 )
 
 func E() {
 	fmt.Println("This is gomodules-e")
-	f2.F2()
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/nobishino/gomodules-e
 
 go 1.22.0
-
-require github.com/nobishino/gomodules-f v1.1.1
